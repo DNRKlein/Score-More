e94be0eebe5d04e6487b47a91885f2a6d499af02
David Klein <David.Klein1994@gmail.com>
2015-04-06 12:47:40 +0200

    Library toegevoegd en class Onderwerp gemaakt

---
+++ ScoreMore/ScoreMoreLib/Onderwerp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ScoreMoreLib
+{
+	public class Onderwerp
+	{
+		private Onderwerp parent;
+		private string titel;
+
+		public Onderwerp (Onderwerp parent, string titel)
+		{
+			this.parent = parent;
+			this.titel = titel;
+		}
+
+		public Onderwerp getParent(){
+			return parent;
+		}
+
+		public void setParent(Onderwerp parent){
+			this.parent = parent;
+		}
+
+		public string getTitel(){
+			return titel;
+		}
+
+		public void setTitel(string titel){
+			this.titel = titel;
+		}
+
+		/// <summary>
+		/// Schrijf Onderwerp naar het Onderwerp file.
+		/// </summary>
+		public void writeToFile(){
+
+		}
+	}
+}
+
