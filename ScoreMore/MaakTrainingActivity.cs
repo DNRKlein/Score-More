abde16f3f1ffae9f2bdd0a93d14f7c60d254a560
Martjr <mart0knoben@gmail.com>
2015-04-13 13:23:03 +0200

    Sync dingen

--- ScoreMore/MaakTrainingActivity.cs
+++ ScoreMore/MaakTrainingActivity.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,55 +42,3 @@ namespace ScoreMore
 		}
 	}
 }
-
-
-﻿
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
-using ScoreMoreLib;
-
-namespace ScoreMore
-{
-	[Activity]			
-	public class MaakTrainingActivity : Activity
-	{
-		private List<Onderwerp> onderwerpenList;
-
-		private Onderwerp pit_1 = new Onderwerp ("Mobile Applications", null);
-		private Onderwerp if_5 = new Onderwerp ("ICT Foundation 5", null);
-		private Onderwerp pit_2 = new Onderwerp ("Embedded Applications", null);
-		private Onderwerp if_6 = new Onderwerp ("ICT Foundation 6", null);
-
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
-			SetContentView (Resource.Layout.MaakTraining);
-
-			onderwerpenList = new List<Onderwerp>();
-			onderwerpenList.Add (pit_1);
-			onderwerpenList.Add (if_5);
-			onderwerpenList.Add (pit_2);
-			onderwerpenList.Add (if_6);
-
-
-
-			ListView listview = FindViewById<ListView> (Resource.Id.listview1);
-			listview.Adapter = new SimpleListItemMulptipleChoiceAdapter (this, onderwerpenList);
-
-			TextView textView = listview.FindViewById<TextView> (Android.Resource.Id.Text1);
-		}
-	}
-}
-
->>>>>>> origin/master
