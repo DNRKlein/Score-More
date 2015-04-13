8d06c4ddafa42179d2efc97a08738a88d9ec10ca
BarryLendertz <lendertz@live.fr>
2015-04-13 11:36:21 +0200

    Toevoeging feedback layout
    Feedback layout toegevoegd met 'werkende' code.

---
+++ ScoreMore/FeedbackActivity.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ScoreMore
+{
+	[Activity (Label = "FeedbackActivity")]			
+	public class FeedbackActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.trainingFeedback);
+
+			Button button_mainMenu = FindViewById<Button> (Resource.Id.but_hoofdmenu);
+
+			// Create your application here
+			button_mainMenu.Click += delegate {
+				StartActivity(typeof(MainMenuActivity));
+			};
+		}
+	}
+}
+
